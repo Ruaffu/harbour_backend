e6bb7fcfb17085b5373d3b7cbff22be87b9b0279
Ruaffu <78867945+Ruaffu@users.noreply.github.com>
2022-06-09 17:09:01 +0200

    edit boat added

--- src/main/java/facades/BoatFacade.java
+++ src/main/java/facades/BoatFacade.java
@@ -1,6 +1,7 @@
 package facades;
 
 import dtos.BoatDTO;
+import dtos.OwnerDTO;
 import entities.Boat;
 import entities.Harbour;
 import entities.Owner;
@@ -8,6 +9,7 @@ import entities.Owner;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.TypedQuery;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BoatFacade
@@ -77,5 +79,39 @@ public class BoatFacade
     public BoatDTO updateBoat(BoatDTO boatDTO)
     {
         EntityManager em = emf.createEntityManager();
+        Boat boat = em.find(Boat.class, boatDTO.getId());
+        boat.setBrand(boatDTO.getBrand());
+        boat.setMake(boatDTO.getMake());
+        boat.setName(boatDTO.getName());
+        boat.setImage(boatDTO.getImage());
+
+        //clears owners
+        boat.getOwners().clear();
+        System.out.println(boatDTO.getOwners());
+
+        for (String own : boatDTO.getOwners())
+        {
+            Owner owner = em.createQuery("SELECT o FROM Owner o WHERE o.name = :own", Owner.class).setParameter("own", own).getSingleResult();
+
+            boat.addOwner(owner);
+
+        }
+        System.out.println(boat.getOwners());
+try
+{
+    TypedQuery<Harbour> query = em.createQuery("SELECT h FROM Harbour h WHERE h.id =:harbourId", Harbour.class);
+    query.setParameter("harbourId", boatDTO.getHarbourID());
+    Harbour harbour = query.getSingleResult();
+    boat.setHarbour(harbour);
+
+    em.getTransaction().begin();
+    em.merge(boat);
+    em.getTransaction().commit();
+
+}finally
+{
+    em.close();
+}
+return new BoatDTO(boat);
     }
 }
